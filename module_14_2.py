d03d47b14ad21c1886b09f48c39ad99897cc8006
Lexa622 <lexa622@gmail.com>
2024-09-26 21:02:03 +0300

    Домашнее задание по теме "Выбор элементов и функции в SQL запросах"

--- module_14_2.py
+++ module_14_2.py
@@ -22,7 +22,7 @@ balance INTEGER NOT NULL
 )
 """)
 cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")     # создание индекса
-"""for i in range(1, 11):
+for i in range(1, 11):
     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
                    (f"User{i}", f"example{i}@gmail.com", f"{i * 10}", "1000"))    # добавление кучей данных
 for i in range(1, 11):
@@ -32,12 +32,12 @@ for i in range(1, 11, 3):
 cursor.execute("SELECT username, email, age, balance FROM Users WHERE not age == ?", (60,))
 users = cursor.fetchall()
 for user in users:
-    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")"""
-# cursor.execute("DELETE FROM Users WHERE id = ?", (6,))  # удаление данных
-cursor.execute("SELECT COUNT(*) FROM Users")
-total_users = cursor.fetchone()[0]
-cursor.execute("SELECT SUM(balance) FROM Users")
-all_balances = cursor.fetchone()[0]
-print(all_balances / total_users)
+    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+cursor.execute("DELETE FROM Users WHERE id = ?", (6,))  # удаление данных
+cursor.execute("SELECT COUNT(*) FROM Users")    # счетаем количество элементов ( строк )
+total_users = cursor.fetchone()[0]      # сохраняем количество элементов
+cursor.execute("SELECT SUM(balance) FROM Users")    # сумма балансов
+all_balances = cursor.fetchone()[0]     # сохраняем сумму балансов
+print(all_balances / total_users)       # средний баланс выводив в консоль
 connection.commit()     # подключение
 connection.close()      # закрываем подключение
