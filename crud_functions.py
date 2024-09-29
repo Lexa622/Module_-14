e4495ebd9adb2508456c596c9b50c06aa4c10e0d
Lexa622 <lexa622@gmail.com>
2024-09-29 11:27:52 +0300

    Домашнее задание по теме "Написание примитивной ORM"

--- crud_functions.py
+++ crud_functions.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def initiate_db():
-    connection = sqlite3.connect("telega.db")     # подключение к базе данных
+    connection = sqlite3.connect("bd.db")     # подключение к базе данных
     cursor = connection.cursor()    # создание курсора
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Products(
@@ -21,12 +21,42 @@ def initiate_db():
                    ("10w40_plus", "maxima_10w40_plus_5l", "300"))    # добавление данных
     cursor.execute("INSERT INTO Products (title, description, price) VALUES (?, ?, ?)",
                    ("auto_lpg_10w40", "maxima_auto_lpg_10w40_4l", "400"))    # добавление данных
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    );
+    """)
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")     # создание индекса
     connection.commit()     # подключение
     connection.close()      # закрываем подключение
 
 
-def get_all_products():     # возвращает все записи из таблицы Products
-    connection = sqlite3.connect('telega.db')
+def add_user(username, email, age):
+    connection = sqlite3.connect("bd.db")     # подключение к базе данных
+    cursor = connection.cursor()    # создание курсора
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f"{username}", f"{email}", f"{age}", "1000"))    # добавление данных
+    connection.commit()     # подключение
+
+
+def is_included(username):
+    connection = sqlite3.connect("bd.db")     # подключение к базе данных
+    cursor = connection.cursor()    # создание курсора
+    users_list = cursor.execute("SELECT * FROM Users")
+    name_f = 0
+    for user in users_list:
+        if username == user[1]:
+            name_f = 1
+    return name_f
+
+
+def get_all_products():     # возвращает все записи из таблицы
+    connection = sqlite3.connect('bd.db')
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM Products')
     all_entries = cursor.fetchall()
